0e501ef0e679e39f1702219ec088d979f90f4881
Houston <ivan.husarov@gmail.com>
2020-05-09 21:09:58 +0300

    unique sorted stream without distinct

--- src/main/java/com/binary_studio/uniq_in_sorted_stream/UniqueSortedStream.java
+++ src/main/java/com/binary_studio/uniq_in_sorted_stream/UniqueSortedStream.java
@@ -1,15 +1,20 @@
 package com.binary_studio.uniq_in_sorted_stream;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public final class UniqueSortedStream {
+    static Set<Long> uniqueSet = new HashSet<>();
 
-	private UniqueSortedStream() {
-	}
+    private UniqueSortedStream() {
+    }
 
-	public static <T> Stream<Row<T>> uniqueRowsSortedByPK(Stream<Row<T>> stream) {
-		// TODO: Ваш код тут. Помните про терминальные операции! :)
-		return null;
-	}
+    public static <T> Stream<Row<T>> uniqueRowsSortedByPK(Stream<Row<T>> stream) {
+        uniqueSet.clear();
+        return stream == null ? Stream.of() : stream.map(Row::getPrimaryId)
+                .filter(x -> uniqueSet.add(x))
+                .map(Row::new);
+    }
 
 }
