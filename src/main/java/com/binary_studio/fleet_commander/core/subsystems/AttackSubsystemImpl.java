66e42f270643951824333f074f2796c607fde1d1
Houston <ivan.husarov@gmail.com>
2020-05-10 23:10:54 +0300

    Attack Subsystem works for now

--- src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
+++ src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
@@ -6,35 +6,58 @@ import com.binary_studio.fleet_commander.core.subsystems.contract.AttackSubsyste
 
 public final class AttackSubsystemImpl implements AttackSubsystem {
 
-	public static AttackSubsystemImpl construct(String name, PositiveInteger powergridRequirments,
-			PositiveInteger capacitorConsumption, PositiveInteger optimalSpeed, PositiveInteger optimalSize,
-			PositiveInteger baseDamage) throws IllegalArgumentException {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+    private String name;
+    private PositiveInteger powergridRequirments;
+    private PositiveInteger capacitorConsumption;
+    private PositiveInteger optimalSpeed;
+    private PositiveInteger optimalSize;
+    private PositiveInteger baseDamage;
 
-	@Override
-	public PositiveInteger getPowerGridConsumption() {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+    private AttackSubsystemImpl(String name, PositiveInteger powergridRequirments
+            , PositiveInteger capacitorConsumption, PositiveInteger optimalSpeed, PositiveInteger optimalSize
+            , PositiveInteger baseDamage) {
+        this.name = name;
+        this.powergridRequirments = powergridRequirments;
+        this.capacitorConsumption = capacitorConsumption;
+        this.optimalSpeed = optimalSpeed;
+        this.optimalSize = optimalSize;
+        this.baseDamage = baseDamage;
+    }
 
-	@Override
-	public PositiveInteger getCapacitorConsumption() {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+    public static AttackSubsystemImpl construct(String name, PositiveInteger powergridRequirments
+            , PositiveInteger capacitorConsumption, PositiveInteger optimalSpeed, PositiveInteger optimalSize
+            , PositiveInteger baseDamage) throws IllegalArgumentException {
+        if (name == null || name.trim().equals(""))
+            throw new IllegalArgumentException("Name should be not null and not empty");
+        return new AttackSubsystemImpl(name, powergridRequirments, capacitorConsumption
+                , optimalSpeed, optimalSize, baseDamage);
+    }
 
-	@Override
-	public PositiveInteger attack(Attackable target) {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+    @Override
+    public PositiveInteger getPowerGridConsumption() {
+        return powergridRequirments;
+    }
 
-	@Override
-	public String getName() {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+    @Override
+    public PositiveInteger getCapacitorConsumption() {
+        return capacitorConsumption;
+    }
+
+    @Override
+    public PositiveInteger attack(Attackable target) {
+        var sizeReductionModifier = (target.getSize().value() >= optimalSize.value())
+                ? 1
+                : (double)target.getSize().value() / (double)optimalSize.value();
+
+        var speedReductionModifier = (target.getCurrentSpeed().value() <= optimalSpeed.value())
+                ? 1
+                : (double)optimalSize.value() / (double) (2 * target.getSize().value());
+        return PositiveInteger.of((int) (baseDamage.value() * Math.min(sizeReductionModifier, speedReductionModifier)));
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 
 }
