50ac22d40d1f9c6f78e1feff0d5c1fa73c124953
Houston <ivan.husarov@gmail.com>
2020-05-11 08:59:59 +0300

    Uniq in sorted update

--- src/main/java/com/binary_studio/uniq_in_sorted_stream/UniqueSortedStream.java
+++ src/main/java/com/binary_studio/uniq_in_sorted_stream/UniqueSortedStream.java
@@ -1,19 +1,15 @@
 package com.binary_studio.uniq_in_sorted_stream;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Stream;
 
 public final class UniqueSortedStream {
-
-	static Set<Long> uniqueSet = new HashSet<>();
 
 	private UniqueSortedStream() {
 	}
 
 	public static <T> Stream<Row<T>> uniqueRowsSortedByPK(Stream<Row<T>> stream) {
-		uniqueSet.clear();
-		return stream == null ? Stream.of() : stream.map(Row::getPrimaryId).filter(x -> uniqueSet.add(x)).map(Row::new);
+		Row.removeLast();
+		return stream == null ? Stream.of() : stream.filter(x -> x.getPrimaryId() != null);
 	}
 
 }
